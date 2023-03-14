8abad7b2be97c598221b4fe999932f448ea71156
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-14 19:01:50 +0300

    gradleRefreshVersions

--- app/src/main/java/com/steve_md/splashscreenapi2023/MainActivity.kt
+++ app/src/main/java/com/steve_md/splashscreenapi2023/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -22,22 +23,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Hello World! Welcome to New Splash screen API")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SplashScreenAPI2023Theme {
-        Greeting("Android")
-    }
-}
